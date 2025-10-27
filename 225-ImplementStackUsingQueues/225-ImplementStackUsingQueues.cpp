f6385a0ac65cf5e538940f9cd44a0a29e97ed6b1
Saiteja Bingi <saiteja1973b@gmail.com>
2025-10-27 19:08:21 +0530

    Create 225-ImplementStackUsingQueues

---
+++ 225-ImplementStackUsingQueues/225-ImplementStackUsingQueues.cpp
@@ -0,0 +1,40 @@
+// Last updated: 10/27/2025, 7:08:21 PM
+class MyStack {
+    queue<int>q;
+public:
+    MyStack() {
+
+    }
+    
+    void push(int x) {
+        int s=q.size();
+        q.push(x);
+        for(int i=0;i<s;i++){
+            q.push(q.front());
+            q.pop();
+        }
+    }
+    
+    int pop() {
+        int val=q.front();
+        q.pop();
+        return val;
+    }
+    
+    int top() {
+        return q.front();
+    }
+    
+    bool empty() {
+        return  q.empty();
+    }
+};
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack* obj = new MyStack();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->top();
+ * bool param_4 = obj->empty();
+ */
