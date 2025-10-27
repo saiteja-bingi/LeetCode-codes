af5a8e340aaf9506da37f6f15ebde05a18c6ff16
Saiteja Bingi <saiteja1973b@gmail.com>
2025-10-27 19:09:43 +0530

    Create 62-UniquePaths

---
+++ 62-UniquePaths/62-UniquePaths.cpp
@@ -0,0 +1,22 @@
+// Last updated: 10/27/2025, 7:09:44 PM
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        vector<int>dp(n,0);
+        dp[0]=1;
+        for(int i=0;i<m;i++){
+            vector<int>temp(n,0);
+            for(int j=0;j<n;j++){
+                if(i==0&&j==0) temp[j]=1;
+                else{
+                    int up=0,left=0;
+                    if(i>0) up=dp[j];
+                    if(j>0) left=temp[j-1];
+                    temp[j]=up+left;
+                }
+            }
+            dp=temp;
+        }
+        return dp[n-1];
+    }
+};
