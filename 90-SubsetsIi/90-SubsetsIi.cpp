c0a6cb8219eab07ff7d49b9e90c94770770d200f
Saiteja Bingi <saiteja1973b@gmail.com>
2025-10-27 19:09:31 +0530

    Create 90-SubsetsIi

---
+++ 90-SubsetsIi/90-SubsetsIi.cpp
@@ -0,0 +1,20 @@
+// Last updated: 10/27/2025, 7:09:31 PM
+class Solution {
+public:
+    void function(vector<int>&arr,vector<vector<int>>&ans,vector<int>&temp,int index){
+        ans.push_back(temp);
+        for(int i =index;i<arr.size();i++){
+            if(i>index&&arr[i]==arr[i-1]) continue;
+            temp.push_back(arr[i]);
+            function(arr,ans,temp,i+1);
+            temp.pop_back();
+        }
+    }
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        vector<vector<int>>ans;
+        vector<int>temp;
+        function(nums,ans,temp,0);
+        return ans;
+    }
+};
