2a51b6ffd92301223a465435620a9ce0114c53b7
Saiteja Bingi <saiteja1973b@gmail.com>
2025-10-27 19:05:57 +0530

    Create 2497-MaximumMatchingOfPlayersWithTrainers

---
+++ 2497-MaximumMatchingOfPlayersWithTrainers/2497-MaximumMatchingOfPlayersWithTrainers.cpp
@@ -0,0 +1,19 @@
+// Last updated: 10/27/2025, 7:05:58 PM
+class Solution {
+public:
+    int matchPlayersAndTrainers(vector<int>& players, vector<int>& trainers) {
+        int n=players.size();
+        int m=trainers.size();
+        sort(players.begin(),players.end());
+        sort(trainers.begin(),trainers.end());
+        int i=0,j=0,count=0;
+        while(i<n&&j<m){
+            if(players[i]<=trainers[j]){
+                count++;
+                i++;
+            }
+            j++;
+        }
+        return count;
+    }
+};
