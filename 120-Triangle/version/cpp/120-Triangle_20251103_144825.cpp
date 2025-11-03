169bd00bd86a9ece08dd9c86a31355b503f4d961
Saiteja Bingi <saiteja1973b@gmail.com>
2025-11-03 14:48:24 +0530

    Create 120-Triangle

---
+++ 120-Triangle/version/cpp/120-Triangle_20251103_144825.cpp
@@ -0,0 +1,31 @@
+// Last updated: 11/3/2025, 2:48:25 PM
+class Solution {
+public:
+    int solve(vector<vector<int>>&arr,int i,int j,int n,vector<vector<int>>&dp){
+        if(i==n-1){
+            return arr[i][j];
+        }
+        if(dp[i][j]!=INT_MAX) return dp[i][j];
+        int p1=arr[i][j]+solve(arr,i+1,j,n,dp);
+        int p2=arr[i][j]+solve(arr,i+1,j+1,n,dp);
+        return dp[i][j]=min(p1,p2);
+        
+    }
+    int minimumTotal(vector<vector<int>>& a) {
+        int n=a.size();
+        vector<int>dp(n,0);
+        for(int j=0;j<n;j++){
+            dp[j]=a[n-1][j];
+        }
+        for(int i=n-2;i>=0;i--){
+            vector<int>temp(n,0);
+            for(int k=i;k>=0;k--){
+                int p1=a[i][k]+dp[k];
+                int p2=a[i][k]+dp[k+1];
+                temp[k]=min(p1,p2);
+            }
+            dp=temp;
+        }
+        return dp[0];
+    }
+};
