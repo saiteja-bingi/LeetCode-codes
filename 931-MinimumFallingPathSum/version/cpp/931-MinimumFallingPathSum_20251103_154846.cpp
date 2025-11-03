eadeab895d69621cacdf279f11968183e2f5852c
Saiteja Bingi <saiteja1973b@gmail.com>
2025-11-03 15:48:45 +0530

    Create 931-MinimumFallingPathSum

---
+++ 931-MinimumFallingPathSum/version/cpp/931-MinimumFallingPathSum_20251103_154846.cpp
@@ -0,0 +1,27 @@
+// Last updated: 11/3/2025, 3:48:46 PM
+class Solution {
+public:
+    int minFallingPathSum(vector<vector<int>>& arr) {
+        int n=arr.size();
+        int ans=INT_MAX;
+        vector<int>dp(n,0);
+        for(int j=0;j<n;j++){
+            dp[j]=arr[0][j];
+        }
+        for(int i=1;i<n;i++){
+            vector<int>temp(n,0);
+            for(int j=0;j<n;j++){
+                int p1=INT_MAX,p2=INT_MAX,p3=INT_MAX;
+                p1=arr[i][j]+dp[j];
+                if(j+1<n) p2=arr[i][j]+dp[j+1];
+                if(j-1>=0) p3=arr[i][j]+dp[j-1];
+                temp[j]=min({p1,p2,p3});
+            }
+            dp=temp;
+        }
+        for(int j=0;j<n;j++){
+            ans=min(ans,dp[j]);
+        }
+        return ans;
+    }
+};
