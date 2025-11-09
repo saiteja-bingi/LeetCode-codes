148430e40f435b80b461fd508820b5375b9101da
Saiteja Bingi <saiteja1973b@gmail.com>
2025-11-09 08:08:43 +0530

    Create

---
+++ problem-1762655923825/version/cpp/problem-1762655923825_20251109_080843.cpp
@@ -0,0 +1,20 @@
+// Last updated: 11/9/2025, 8:08:43 AM
+class Solution {
+public:
+    int minimumDistance(vector<int>& arr) {
+        int n=arr.size();
+        int ans=INT_MAX;
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                for(int k=j+1;k<n;k++){
+                    if(arr[i]==arr[j]&&arr[j]==arr[k]&&arr[k]==arr[i]){
+                        int c=abs(i-j)+abs(j-k)+abs(k-i);
+                        ans=min(ans,c);
+                    }
+                }
+            }
+        }
+        if(ans==INT_MAX) return -1;
+        return ans;
+    }
+};
