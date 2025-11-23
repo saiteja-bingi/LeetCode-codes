c6c4553cb89c16b64e3d45b67a976bf082640ae5
Saiteja Bingi <saiteja1973b@gmail.com>
2025-11-23 09:19:03 +0530

    Create

---
+++ problem-1763869743369/version/cpp/problem-1763869743369_20251123_091903.cpp
@@ -0,0 +1,23 @@
+// Last updated: 11/23/2025, 9:19:03 AM
+class Solution {
+public:
+    int maxBalancedSubarray(vector<int>& nums) {
+        int n=nums.size();
+        int eo=0;
+        int ans=0,x=0;
+        unordered_map<string,int>mp;
+        mp["0_0"]=-1;
+        for(int i=0;i<n;i++){
+            x^=nums[i];
+            if(nums[i]%2==0) eo++;
+            else eo--;
+            string k=to_string(x)+"_"+to_string(eo);
+            if(mp.count(k)){
+                ans=max(ans,i-mp[k]);
+            }
+            else mp[k]=i;
+        }
+        return ans;
+        
+    }
+};
