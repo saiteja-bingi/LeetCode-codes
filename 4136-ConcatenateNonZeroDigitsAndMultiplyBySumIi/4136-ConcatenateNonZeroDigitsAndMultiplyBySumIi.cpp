a302a0ae1c26afeb12acedfdce2ec7e63b4245e3
Saiteja Bingi <saiteja1973b@gmail.com>
2025-12-01 19:13:01 +0530

    Create 4136-ConcatenateNonZeroDigitsAndMultiplyBySumIi

---
+++ 4136-ConcatenateNonZeroDigitsAndMultiplyBySumIi/4136-ConcatenateNonZeroDigitsAndMultiplyBySumIi.cpp
@@ -0,0 +1,47 @@
+// Last updated: 12/1/2025, 7:13:01 PM
+class Solution {
+public:
+    const long long mod = 1000000007;
+    vector<int> sumAndMultiply(string s, vector<vector<int>>& queries) {
+        int n = s.size();
+        vector<long long> presum(n, 0), premod(n, 0);
+        vector<int> precount(n, 0);
+        int nzCount = 0;
+        for (char c : s) if (c != '0') nzCount++;
+
+        vector<long long> pow(nzCount + 1, 1);
+        long long sum = 0, num = 0;
+        int nonZeroIndex = 0;
+        for (int i = 0; i < n; i++) {
+            if (s[i] != '0') {
+                sum += (s[i] - '0');
+                num = (num * 10 + (s[i] - '0')) % mod;
+                nonZeroIndex++;
+                pow[nonZeroIndex] = (pow[nonZeroIndex - 1] * 10) % mod;
+            }
+            presum[i] = sum;
+            premod[i] = num;
+            if (i == 0) {
+                precount[i] = (s[i] != '0') ? 1 : 0;
+            } else {
+                precount[i] = precount[i-1] + ((s[i] != '0') ? 1 : 0);
+            } 
+        }
+
+        vector<int> ans;
+        for (auto &q : queries) {
+            int l = q[0], r = q[1];
+            long long digitSum = presum[r] - (l > 0 ? presum[l - 1] : 0);
+
+            long long number = premod[r];
+            if (l > 0) {
+                int len = precount[r] - precount[l - 1];
+                number = (number - premod[l - 1] * pow[len] % mod + mod) % mod;
+            }
+
+            ans.push_back((number * digitSum) % mod);
+        }
+
+        return ans;
+    }
+};
