9fa4f22870840be27489005a59e6efe949df443b
Saiteja Bingi <saiteja1973b@gmail.com>
2026-04-15 00:47:10 +0530

    Create 130-SurroundedRegions

---
+++ 130-SurroundedRegions/130-SurroundedRegions.cpp
@@ -0,0 +1,33 @@
+// Last updated: 15/04/2026, 00:47:10
+class Solution {
+public:
+    void dfs(auto &board,int i,int j){
+        int n=board.size();
+        int m=board[0].size();
+        if(i<0||j<0||i>=n||j>=m||board[i][j]!='O') return;
+        if(board[i][j]=='O') board[i][j]='#';
+        int dx[4]={1,-1,0,0};
+        int dy[4]={0,0,1,-1};
+        for(int x=0;x<4;x++){
+            dfs(board,i+dx[x],j+dy[x]);
+        }
+    }
+    void solve(vector<vector<char>>& board) {
+        int n=board.size();
+        int m=board[0].size();
+        for(int i=0;i<n;i++){
+            if(board[i][0]=='O') dfs(board,i,0);
+            if(board[i][m-1]=='O') dfs(board,i,m-1);
+        }
+        for(int j=0;j<m;j++){
+            if(board[0][j]=='O') dfs(board,0,j);
+            if(board[n-1][j]=='O') dfs(board,n-1,j);
+        }
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(board[i][j]=='O') board[i][j]='X';
+                else if(board[i][j]=='#')  board[i][j]='O';
+            }
+        }
+    }
+};
