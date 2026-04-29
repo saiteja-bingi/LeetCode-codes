30fa4f9defa0fe9d2f30ec5adc5d0b3ca5c7ffb5
Saiteja Bingi <saiteja1973b@gmail.com>
2026-04-29 16:47:51 +0530

    Create 2033-MinimumOperationstoMakeaUni-ValueGrid

---
+++ 2033-MinimumOperationstoMakeaUni-ValueGrid/version/cpp/2033-MinimumOperationstoMakeaUni-ValueGrid_20260429_164751.cpp
@@ -0,0 +1,24 @@
+// Last updated: 29/4/2026, 4:47:51 pm
+1class Solution {
+2public:
+3    int minOperations(vector<vector<int>>& grid, int x) {
+4        int n=grid.size(),m=grid[0].size();
+5        if(n==1&&m==1) return 0;
+6        vector<int>arr;
+7        int sum=0;
+8        for(int i=0;i<n;i++) for(int j=0;j<m;j++) {
+9            arr.push_back(grid[i][j]);
+10            sum+=grid[i][j];
+11        }
+12        sort(arr.begin(),arr.end());
+13        n=arr.size();
+14        int val=arr[(n+1)/2-1];
+15        int c=0;
+16        for(int i=0;i<n;i++){
+17            if(arr[i]%x!=arr[0]%x) return -1;
+18            c+=abs((val-arr[i])/x);
+19        }
+20        return c;
+21
+22    }
+23};
