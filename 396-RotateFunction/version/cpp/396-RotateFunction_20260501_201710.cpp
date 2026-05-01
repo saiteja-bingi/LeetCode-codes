7a6b27b5a2f28610f2bb4c1c9d5d3d80952a2276
Saiteja Bingi <saiteja1973b@gmail.com>
2026-05-01 20:17:10 +0530

    Create 396-RotateFunction

---
+++ 396-RotateFunction/version/cpp/396-RotateFunction_20260501_201710.cpp
@@ -0,0 +1,20 @@
+// Last updated: 1/5/2026, 8:17:10 pm
+1#define ll long long
+2class Solution {
+3public:
+4    int maxRotateFunction(vector<int>& arr) {
+5        ll z = 0;
+6        ll s = arr[0];
+7        for(int i = 1;i<arr.size();i++){
+8            z+=arr[i]*i;
+9            s+=arr[i];
+10        }
+11        ll pre = z;
+12        for(int i = 1;i<arr.size();i++){
+13            ll cur = pre + s - arr.size()*arr[arr.size()-i];
+14            z = max(z,cur);
+15            pre = cur;
+16        }
+17        return z;
+18    }
+19};
