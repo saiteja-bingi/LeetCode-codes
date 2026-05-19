a6f6c40d232f12428e5a58e10a298ff5962d4e11
Saiteja Bingi <saiteja1973b@gmail.com>
2026-05-19 10:32:46 +0530

    Create 3908-ValidDigitNumber

---
+++ 3908-ValidDigitNumber/version/cpp/3908-ValidDigitNumber_20260519_103245.cpp
@@ -0,0 +1,14 @@
+// Last updated: 19/5/2026, 10:32:45 am
+1class Solution {
+2public:
+3    bool validDigit(int n, int x) {
+4        int c=0,y=0;
+5        while(n>0){
+6            c+=(n%10==x);
+7            y=(n%10==x);
+8            n/=10;
+9        }
+10        cout<<y<<" "<<c<<endl;
+11        return (!y)&&c;
+12    }
+13};
