3d921e9cde210371c9649cf7c98466e7d4a9d80b
Saiteja Bingi <saiteja1973b@gmail.com>
2026-07-19 08:40:34 +0530

    Create

---
+++ problem-1784430633883/version/cpp/problem-1784430633883_20260719_084033.cpp
@@ -0,0 +1,7 @@
+// Last updated: 19/7/2026, 8:40:33 am
+1class Solution {
+2public:
+3    bool canReach(vector<int>& start, vector<int>& target) {
+4        return ((start[0]+start[1])%2==(target[0]+target[1])%2);
+5    }
+6};
