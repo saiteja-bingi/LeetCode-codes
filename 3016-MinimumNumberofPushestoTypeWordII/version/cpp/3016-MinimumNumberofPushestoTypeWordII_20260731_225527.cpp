4736ef75826d07072b157d1c7f0c1577c615e284
Saiteja Bingi <saiteja1973b@gmail.com>
2026-07-31 22:55:28 +0530

    Create 3016-MinimumNumberofPushestoTypeWordII

---
+++ 3016-MinimumNumberofPushestoTypeWordII/version/cpp/3016-MinimumNumberofPushestoTypeWordII_20260731_225527.cpp
@@ -0,0 +1,20 @@
+// Last updated: 31/7/2026, 10:55:27 pm
+1class Solution {
+2public:
+3    int minimumPushes(string word) {
+4        unordered_map<char,int>mp;
+5        for(auto it:word) mp[it]++;
+6        int c=0,ans=0,k=1;
+7        vector<int>vec;
+8        for(auto &[x,y]:mp) vec.push_back(y);
+9        sort(vec.begin(),vec.end(),greater<int>());
+10        for(auto y:vec){
+11            if(c==8){
+12                c=0;k++;
+13            }
+14            c++;
+15            ans+=k*y;
+16        }
+17        return ans;
+18    }
+19};
