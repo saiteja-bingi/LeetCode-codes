d6de8b05d6bca37e0b85194cd67bf9b4a1c2b84d
Saiteja Bingi <saiteja1973b@gmail.com>
2026-08-19 23:57:36 +0530

    Create 4384-ElevatorRequestsI

---
+++ 4384-ElevatorRequestsI/4384-ElevatorRequestsI.cpp
@@ -0,0 +1,11 @@
+// Last updated: 19/8/2026, 11:57:36 pm
+class Solution {
+public:
+    int elevatorRequests(int n, vector<int>& requests) {
+        int sum=requests[0];
+        for(int i=1;i<requests.size();i++){
+            sum+=abs(requests[i]-requests[i-1]);
+        }
+        return sum;
+    }
+};
